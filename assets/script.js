c88201e9566b9454236ea380512d195fd9c5e40f
JacksonAhumada <jacksonahumada@gmail.com>
2022-09-25 19:53:57 -0600

    lat lon stored. weather api cant be called.

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,36 @@
 var APIKey = "058d9811a06090fb1760d402c75df68e";
 var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
 
 var searchBtn = document.getElementById("searchBtn");
-var cityText = document.getElementById("cityText")
+var cityText = document.getElementById("cityText");
 
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  var cityString = cityText.value;
+  console.log(cityString);
 
-searchBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    var cityString = cityText.value
-    console.log(cityString)
-
-    
-    
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityString}&limit=5&appid=${APIKey}`)
-        .then((data) => data.json())
-        .then((data) => console.log(data))
-        .catch(err => console.error(err));
-        
-        
+  fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${cityString}&limit=5&appid=${APIKey}`
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      console.log(data);
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      console.log(lat);
+      console.log(lon);
+    })
+    .catch((err) => console.error(err));
+fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${APIKey}`
+)
+.then((data) => data.json())
+    .then((data) => {
+      console.log(data);
+    });
 });
-
-// var lat = <span id="lat"></span>
-// var lon = <span id="lon"></span>
